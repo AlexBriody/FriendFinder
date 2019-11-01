1b4b24ab9cae5af91fbf467553a73c7e9c2cc4bd
Alex Briody <alexbriody@gmail.com>
2019-11-01 15:46:02 -0400

    friends.js data and .post apiRoutes.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,88 +3,32 @@
 
 
 var friendsArray = [
-    {
-      name: "Han Solo",
-      photo: "https://media0.giphy.com/media/3o7abERY79vph15FV6/giphy_s.gif?cid=81590a1c35ee702f58cd6ba49316765a1c8ac465e865066e&rid=giphy_s.gif",
-      scores: [
-        "5",
-        "4",
-        "3",
-        "2",
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "4"
-      ]
-    },
-    {
-        name: "Princess Leia",
-        photo: "https://media3.giphy.com/media/3ohuPfGENOtDzsEaXK/giphy_s.gif?cid=81590a1c6452b88ef629987319e9d296a4ae8e24cf90795e&amp;rid=giphy_s.gif",
-        scores: [
-          "3",
-          "5",
-          "1",
-          "5",
-          "4",
-          "3",
-          "3",
-          "1",
-          "5",
-          "4"
-        ]
-      },
-      {
-        name: "Luke Skywalker",
-        photo: "https://media1.giphy.com/media/l3fZZuwvELduur3sA/giphy-downsized_s.gif?cid=81590a1c2f570fb921dca9c2d97f6cc821f648cfb068a817&rid=giphy-downsized_s.gif",
-        scores: [
-          "3",
-          "5",
-          "1",
-          "5",
-          "4",
-          "1",
-          "2",
-          "1",
-          "5",
-          "4"
-        ]
-      },
-      {
-        name: "Jabba the Hut",
-        photo: "https://media1.giphy.com/media/l1ugtlSwK8or4vGik/giphy_s.gif?cid=81590a1ca6ed572cb83a6855b4192fa848a5d5799e241098&rid=giphy_s.gif",
-        scores: [
-          "4",
-          "2",
-          "2",
-          "5",
-          "4",
-          "3",
-          "3",
-          "1",
-          "5",
-          "4"
-        ]
-      },
-      {
-        name: "Obi Wan Kenobi",
-        photo: "https://media2.giphy.com/media/l2JJKs3I69qfaQleE/giphy-downsized_s.gif?cid=81590a1ce351af7fb1b0429b4a98592fc86835ea93fa537e&rid=giphy-downsized_s.gif",
-        scores: [
-          "2",
-          "4",
-          "3",
-          "2",
-          "1",
-          "3",
-          "3",
-          "4",
-          "1",
-          "5"
-        ]
-      }
-  ];
-  
-  // Note how we export the array. This makes it accessible to other files using require.
-  module.exports = friendsArray;
-  
+  {
+    name: "Han Solo",
+    photo: "https://media0.giphy.com/media/3o7abERY79vph15FV6/giphy_s.gif?cid=81590a1c35ee702f58cd6ba49316765a1c8ac465e865066e&rid=giphy_s.gif",
+    scores: [5, 4, 3, 2, 1, 2, 3, 4, 5, 4]
+  },
+  {
+    name: "Princess Leia",
+    photo: "https://media3.giphy.com/media/3ohuPfGENOtDzsEaXK/giphy_s.gif?cid=81590a1c6452b88ef629987319e9d296a4ae8e24cf90795e&amp;rid=giphy_s.gif",
+    scores: [3, 5, 1, 5, 4, 3, 3, 1, 5, 2]
+  },
+  {
+    name: "Luke Skywalker",
+    photo: "https://media1.giphy.com/media/l3fZZuwvELduur3sA/giphy-downsized_s.gif?cid=81590a1c2f570fb921dca9c2d97f6cc821f648cfb068a817&rid=giphy-downsized_s.gif",
+    scores: [3, 5, 1, 5, 4, 1, 2, 1, 5, 4]
+  },
+  {
+    name: "Jabba the Hut",
+    photo: "https://media1.giphy.com/media/l1ugtlSwK8or4vGik/giphy_s.gif?cid=81590a1ca6ed572cb83a6855b4192fa848a5d5799e241098&rid=giphy_s.gif",
+    scores: [4, 2, 2, 5, 4, 3, 3, 1, 5, 4]
+  },
+  {
+    name: "Obi Wan Kenobi",
+    photo: "https://media2.giphy.com/media/l2JJKs3I69qfaQleE/giphy-downsized_s.gif?cid=81590a1ce351af7fb1b0429b4a98592fc86835ea93fa537e&rid=giphy-downsized_s.gif",
+    scores: [2, 4, 3, 2, 1, 3, 3, 4, 1, 5]
+  }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,29 +11,53 @@ var friendsData = require("../data/friends.js");
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/friends", function(req, res) {
+  app.get("/api/friends", function (req, res) {
     res.json(friendsData);
   }); //closing for app.get
 
-  for (var i=0; i < friendsData.length; i++) {
-  console.log("friendsData: " + "\nfriend's name: " + friendsData[i].name + "\nfriend's photo: " + friendsData[i].photo + "\nfriend's scores: " + friendsData[i].scores)
+  for (var i = 0; i < friendsData.length; i++) {
+    console.log([i] + " friendsData: " + "\nfriend's name: " + friendsData[i].name + "\nfriend's photo: " + friendsData[i].photo + "\nfriend's scores: " + friendsData[i].scores)
   }
 
   app.post('/api/friends', function (req, res) {
-    
 
+    var newFriendData = req.body;
 
+    var differencesSum = 0;
+
+    var currentMinimum = 50;
+
+    for (var i = 0; i < friendsData.length; i++) {
+
+      for (var j = 0; j < newFriendData.scores[j].length; j++) {
+
+        var singleDifference = Math.abs(parseInt(newFriendData.scores[j]) - parseInt(friendsData[i].scores[j]));
+
+        differencesSum += singleDifference;
+
+      }//closing for for loop [j]
+
+      if (differencesSum < currentMinimum) {
+
+        var foundFriend = friendsData[i];
+
+        currentMinimum = differencesSum;
+
+      }//closing for if statement
+
+    };//closing for for loop [i]
+
+    res.json(foundFriend);
+
+    friendsData.push(newFriendData);
 
   }); //closing for app.post
 
-  
-
-  
 }; //closing for module.exports and function(app)
